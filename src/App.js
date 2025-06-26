898bc9acbab2e559bafe5697d6d93e52e520ffc8
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2025-06-26 13:36:08 +0200

    move local files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './img/';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>🚧Work In Progress🔨</h1>
+        <p>Hello current or future Reading Nook member! Excuse the mess, we're currently giving our website a makeover during the summer. You can follow the progress on <a href="https://github.com/gorillagripcore/readingnook.se" target="_blank" rel="noopener noreferrer">Github</a></p>
+        <p>//Ebba & Alexandra</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
