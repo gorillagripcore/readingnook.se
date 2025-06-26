90a4f946caef650478c7eb5269d192efe2ad21c9
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2025-06-26 13:52:49 +0200

    Added adobe fonts
    and made sure it actually works hahaha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './img/';
 import './App.css';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
       <div className='container'>
         <h1>🚧Work In Progress🔨</h1>
         <p>Hello current or future Reading Nook member! Excuse the mess, we're currently giving our website a makeover during the summer. You can follow the progress on <a href="https://github.com/gorillagripcore/readingnook.se" target="_blank" rel="noopener noreferrer">Github</a></p>
-        <p>//Ebba & Alexandra</p>
+        <p className='names'>//Ebba & Alexandra</p>
       </div>
     </div>
   );
